5626d45b5ada20cdb8189654a19ff7a1d5381bd4
wucangeo <wucangeo@gmail.com>
2017-12-15 00:02:39 +0800

    完善api.user的增删查改

--- src/api/user.js
+++ src/api/user.js
@@ -1,27 +1,58 @@
+import Cookies from 'js-cookie'
 import axios from 'axios'
 
+axios.defaults.baseURL = CONFIG.API_V1
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.patch['Content-Type'] = 'application/json'
+
 module.exports = {
-  list: async function() {
+  list: async function(query) {
+    let access_token = Cookies.get('access_token')
     return axios({
-      url: CONFIG.API_V1 + '/user',
-      method: 'get'
+      url: '/user',
+      method: 'get',
+      headers: { 'x-access-token': access_token },
+      params: query
+    })
+  },
+  get: async function(data_id) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: `/user/${data_id}`,
+      method: 'get',
+      headers: { 'x-access-token': access_token }
     })
   },
   login: function(data) {
     return axios({
-      url: CONFIG.API_V1 + '/user/login',
+      url: '/user/login',
       method: 'post',
-      data: data,
-      headers: {
-        'Content-type': 'application/json'
-      }
+      data: data
+    })
+  },
+  update: function(updates, data_id) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: `/user/${data_id}`,
+      method: 'patch',
+      headers: { 'x-access-token': access_token },
+      data: updates
     })
   },
   delete: function(ids) {
+    let access_token = Cookies.get('access_token')
     return axios({
-      url: CONFIG.API_V1 + '/user',
-      method: 'delete',
-      headers: { 'content-type': 'application/json' }
+      url: '/user',
+      headers: { 'x-access-token': access_token },
+      method: 'delete'
+    })
+  },
+  verify: function() {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: '/user/verify',
+      headers: { 'x-access-token': access_token },
+      method: 'get'
     })
   }
 }
