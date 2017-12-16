1ea28debc5e453cd715f3fdbdc8cd87e4ec41903
wucangeo <wucangeo@gmail.com>
2017-12-16 13:53:57 +0800

    修改网站的title

--- src/libs/util.js
+++ src/libs/util.js
@@ -5,7 +5,7 @@ import packjson from '../../package.json'
 
 let util = {}
 util.title = function(title) {
-  title = title || 'iView admin'
+  title = title || '黄土高原水土保持试验站后台管理系统'
   window.document.title = title
 }
 
