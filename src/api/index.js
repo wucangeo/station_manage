48c8daf935fbef746f6fde926747b8298c9c623b
wucangeo <wucangeo@gmail.com>
2017-12-17 17:30:48 +0800

    添加station和upload接口调用

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
 import * as user from './user'
+import * as station from './station'
+import * as upload from './upload'
 
 const apis = {
+  upload,
+  station,
   user
 }
 
---
+++ src/api/station.js
@@ -0,0 +1,55 @@
+import Cookies from 'js-cookie'
+import axios from 'axios'
+
+axios.defaults.baseURL = CONFIG.API_V1
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.patch['Content-Type'] = 'application/json'
+
+let salt = 'bnu'
+
+module.exports = {
+  list: async function(query) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: '/station',
+      method: 'get',
+      headers: { 'x-access-token': access_token },
+      params: query
+    })
+  },
+  get: async function(data_id) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: `/station/${data_id}`,
+      method: 'get',
+      headers: { 'x-access-token': access_token }
+    })
+  },
+  create: function(item) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: `/station`,
+      method: 'post',
+      headers: { 'x-access-token': access_token },
+      data: item
+    })
+  },
+  update: function(updates, data_id) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: `/station/${data_id}`,
+      method: 'patch',
+      headers: { 'x-access-token': access_token },
+      data: updates
+    })
+  },
+  delete: function(ids) {
+    let access_token = Cookies.get('access_token')
+    return axios({
+      url: '/station',
+      headers: { 'x-access-token': access_token },
+      method: 'delete',
+      data: { ids: ids }
+    })
+  }
+}
