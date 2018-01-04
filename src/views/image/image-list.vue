cefcbfdc36e7f599908894e0ba5d61e111c03f80
wucangeo <wucangeo@gmail.com>
2018-01-04 22:07:51 +0800

    完成[生态图片]模块

--- src/views/image/image-list.vue
+++ src/views/image/image-list.vue
@@ -1,47 +1,47 @@
 <template>
   <div>
-    <!-- <Card :padding="10"> -->
-    <Row type="flex" justify="end">
-      <span>
-        <Button type="primary" @click="is_add_modal=true" icon="plus">添加</Button>
-      </span>
-    </Row>
     <Row type="flex" justify="start" class="image-list-row">
       <Col class="image-item" v-for="item in image_list">
       <div @mouseover="mouseHover(item)" @mouseleave="mouseLeave(item)">
         <Card :padding="10">
-          <p slot="title">{{item.name}}</p>
-          <p slot="extra" v-show="item.data_id==cur_hover">
-            <Icon type="edit" class="image-extra-edit"></Icon>
-            <Icon type="close" class="image-extra-remove"></Icon>
+          <p slot="title" v-if="cur_edit!=item.data_id">{{item.name}}</p>
+          <p slot="title" v-if="cur_edit==item.data_id">
+            <Input type="text" v-model="newImageName" :value="item.name" size="small" placeholder="请输入图片名称" style="width:200px;margin-right:10px;"></Input>
+            <Icon type="checkmark" @click.native="updateImageName(item)" class="image-extra-edit"></Icon>
+            <Icon type="close-round" @click.native="setEditCancel" class="image-extra-edit"></Icon>
           </p>
-          <p>
+          <p slot="extra" v-show="item.data_id==cur_hover&&cur_edit!=item.data_id">
+            <Icon type="edit" @click.native="setEdit(item)" class="image-extra-edit"></Icon>
+            <Icon type="close" @click.native="deleteImage(item)" class="image-extra-remove"></Icon>
+          </p>
+          <p class="image-container">
             <router-link :to="{ name: 'scientificImageView', params: { type: 1,data_id:item.data_id }}"><img :src="item.path | getAbsolutePath"></router-link>
           </p>
           <p class="image-item-footer">{{item.updated_at|getENDatetime}} </p>
-          <!-- <Button type="error">Delete</Button> -->
         </Card>
       </div>
       </Col>
+      <Col class="image-item">
+      <Card :padding="10">
+        <p slot="title">上传新的图片
+        </p>
+        <p class="upload-image">
+          <Upload multiple type="drag" :action="uploadURL" :on-success="uploadSuccess" :headers="uploadHeader" :data="uploadData">
+            <div style="padding: 20px 0">
+              <Icon type="plus-round" size="52" style="color: #3399ff"></Icon>
+              <p>点击这里上传</p>
+            </div>
+          </Upload>
+        </p>
+      </Card>
+      </Col>
     </Row>
-    <!-- </Card> -->
-    <Modal v-model="is_add_modal" title="添加图片" :width="400" @on-ok="ok" @on-cancel="cancel">
-      <p class="image-item-title">
-        <Input placeholder="请输入标题..."></Input>
-      </p>
-      <p class="upload-image">
-        <Upload multiple type="drag" action="//jsonplaceholder.typicode.com/posts/">
-          <div style="padding: 20px 0">
-            <Icon type="plus-round" size="52" style="color: #3399ff"></Icon>
-            <p>点击这里上传</p>
-          </div>
-        </Upload>
-      </p>
-      <!-- <Button type="error">Delete</Button> -->
-    </Modal>
   </div>
 </template>
 <script>
+import Cookies from 'js-cookie'
+let access_token = Cookies.get('access_token')
+
 export default {
   name: 'image',
   data() {
@@ -50,13 +50,22 @@ export default {
         key: '',
         type: 1,
         offset: 0,
-        limit: 10,
-        order: 1,
+        limit: -1,
+        order: 0,
         order_by: 'data_id'
       },
       image_list: [],
       cur_hover: null,
-      is_add_modal: false
+      is_add_modal: false,
+      uploadHeader: { 'x-access-token': access_token },
+      uploadData: { type: 1 },
+      cur_edit: null,
+      newImageName: ''
+    }
+  },
+  computed: {
+    uploadURL: function() {
+      return CONFIG.API_V1 + '/image/upload'
     }
   },
   methods: {
@@ -78,11 +87,51 @@ export default {
     mouseLeave(item) {
       this.cur_hover = null
     },
-    ok() {
-      this.$Message.info('Clicked ok')
+    uploadSuccess() {
+      this.list()
     },
-    cancel() {
-      this.$Message.info('Clicked cancel')
+    setEdit(item) {
+      this.cur_edit = item.data_id
+      this.newImageName = item.name
+    },
+    setEditCancel() {
+      this.cur_edit = null
+    },
+    async deleteImage(item) {
+      let response = await this.apis.image.delete([item.data_id])
+      let result = response.data
+      if (result.code === 0) {
+        this.$Message.error({
+          content: result.msg,
+          duration: 1.5
+        })
+      } else {
+        this.$Message.success({
+          content: result.msg,
+          duration: 1.5
+        })
+        this.list()
+      }
+    },
+    async updateImageName(item, name) {
+      let response = await this.apis.image.update(
+        { name: this.newImageName },
+        item.data_id
+      )
+      let result = response.data
+      if (result.code === 0) {
+        this.$Message.error({
+          content: result.msg,
+          duration: 1.5
+        })
+      } else {
+        this.$Message.success({
+          content: result.msg,
+          duration: 1.5
+        })
+        item.name = this.newImageName
+        this.setEditCancel()
+      }
     }
   },
   mounted() {
@@ -92,11 +141,12 @@ export default {
 </script>
 <style lang="less">
 .image-list-row {
-  overflow-y: auto;
+  // overflow-y: auto;
   .image-item {
     width: 274px;
     height: 250px;
     margin: 8px 4px;
+    overflow: hidden;
     .ivu-card {
       height: 100%;
       width: 100%;
@@ -104,11 +154,15 @@ export default {
         padding: 10px;
       }
       .image-item-footer {
-        margin: 10px;
-        color: #828282;
+        margin-top: 3px;
+        color: #a5a5a5;
+        font-size: 10px;
       }
       .ivu-upload {
-        height: 10px;
+        height: 140px;
+      }
+      .image-item-title {
+        margin-bottom: 10px;
       }
       .image-extra {
         display: none;
@@ -125,20 +179,24 @@ export default {
       .image-body {
         cursor: pointer;
       }
+      .image-container {
+        width: 100%;
+        height: 161px;
+        overflow: hidden;
+        text-align: center;
+        img {
+          height: auto;
+          width: 100%;
+          max-width: 100%;
+          // max-height: 100%;
+        }
+      }
     }
     .image-item:hover {
       .image-extra {
         display: inherit;
       }
     }
-
-    img {
-      max-width: 100%;
-      max-height: 100%;
-    }
   }
-}
-.image-item-title {
-  margin-bottom: 10px;
 }
 </style>
