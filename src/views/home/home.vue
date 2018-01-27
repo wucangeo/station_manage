e72d48bd902d8c1045cbeebe799bf97406f2d2b4
wucan <wucan@geo-compass.com>
2018-01-27 14:19:54 +0800

    修复地图符号大小以及折线图数值显示

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -106,64 +106,64 @@
   </div>
 </template>
 <script>
-import Cookies from 'js-cookie'
-import ECharts from 'vue-echarts/components/ECharts'
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/chart/line'
-import 'echarts/lib/chart/scatter'
-import 'echarts/lib/chart/effectScatter'
-import 'echarts/lib/chart/map'
-import 'echarts/lib/component/visualMap'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/geo'
-import 'echarts/lib/component/legend'
-import 'echarts/lib/component/title'
+import Cookies from "js-cookie";
+import ECharts from "vue-echarts/components/ECharts";
+import "echarts/lib/chart/bar";
+import "echarts/lib/chart/line";
+import "echarts/lib/chart/scatter";
+import "echarts/lib/chart/effectScatter";
+import "echarts/lib/chart/map";
+import "echarts/lib/component/visualMap";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/geo";
+import "echarts/lib/component/legend";
+import "echarts/lib/component/title";
 
-import geoCoordMap from './geoCoordMap.json'
-import chinaJson from './chinaMap.json'
-ECharts.registerMap('china', chinaJson)
+import geoCoordMap from "./geoCoordMap.json";
+import chinaJson from "./chinaMap.json";
+ECharts.registerMap("china", chinaJson);
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     chart: ECharts
   },
   data() {
     return {
       lastLogin: {
-        username: '',
-        name: '',
-        time: '未知',
-        city: '未知'
+        username: "",
+        name: "",
+        time: "未知",
+        city: "未知"
       },
       topCount: [
         {
-          icon: 'ios-eye',
-          color: '#2d8cf0',
+          icon: "ios-eye",
+          color: "#2d8cf0",
           count: 0,
-          text: '今日访问总量'
+          text: "今日访问总量"
         },
         {
-          icon: 'happy',
-          color: '#64d572',
+          icon: "happy",
+          color: "#64d572",
           count: 0,
-          text: '今日访问用户'
+          text: "今日访问用户"
         },
         {
-          icon: 'person-stalker',
-          color: '#ffd572',
+          icon: "person-stalker",
+          color: "#ffd572",
           count: 0,
-          text: '今日管理员登录'
+          text: "今日管理员登录"
         },
         {
-          icon: 'wand',
-          color: '#f25e43',
+          icon: "wand",
+          color: "#f25e43",
           count: 0,
-          text: '今日新增和更新'
+          text: "今日新增和更新"
         }
       ],
       initOptions: {
-        renderer: 'canvas'
+        renderer: "canvas"
       },
       barUrlCount: {
         items: [],
@@ -171,12 +171,12 @@ export default {
       }, //按模块统计， 柱状图
       lineUrlCount: {
         items: [
-          '台站信息',
-          '台站风光',
-          '新闻公告',
-          '数据资源',
-          '科研成果',
-          '科研项目'
+          "台站信息",
+          "台站风光",
+          "新闻公告",
+          "数据资源",
+          "科研成果",
+          "科研项目"
         ],
         data: {
           station: [0, 0, 0, 0, 0, 0, 0],
@@ -191,59 +191,60 @@ export default {
       },
       tableColumn: [
         {
-          title: '城市',
-          key: 'name'
+          title: "城市",
+          key: "name"
         },
         {
-          title: '浏览量',
-          key: 'value',
+          title: "浏览量",
+          key: "value",
           width: 80
         }
       ],
       tableData: [],
-      mapData: [] //地图数据
-    }
+      mapData: [], //地图数据
+      maxMapCount: 10 //地图中城市最大浏览量
+    };
   },
   computed: {
     barOption() {
       return {
         tooltip: {
-          trigger: 'item'
+          trigger: "item"
         },
         yAxis: {
-          type: 'category',
+          type: "category",
           data: this.barUrlCount.items
         },
         xAxis: {
-          type: 'value',
+          type: "value",
           axisLabel: {
             show: true
           }
         },
         grid: {
-          top: '0',
-          left: '0',
-          right: '10px',
-          bottom: '0',
+          top: "0",
+          left: "0",
+          right: "10px",
+          bottom: "0",
           containLabel: true
         },
         series: [
           {
-            type: 'bar',
-            name: '浏览量',
+            type: "bar",
+            name: "浏览量",
             data: this.barUrlCount.values
           }
         ]
-      }
+      };
     },
     lineOption() {
       return {
         tooltip: {
-          trigger: 'axis',
+          trigger: "axis",
           axisPointer: {
-            type: 'cross',
+            type: "cross",
             label: {
-              backgroundColor: '#6a7985'
+              backgroundColor: "#6a7985"
             }
           }
         },
@@ -256,102 +257,102 @@ export default {
           }
         },
         grid: {
-          top: '5px',
-          left: '5px',
-          right: '15px',
-          bottom: '5px',
+          top: "5px",
+          left: "5px",
+          right: "15px",
+          bottom: "5px",
           containLabel: true
         },
         xAxis: [
           {
-            type: 'category',
+            type: "category",
             boundaryGap: false,
             data: this.lineUrlCount.dateItems
           }
         ],
         yAxis: [
           {
-            type: 'value'
+            type: "value"
           }
         ],
         series: [
           {
-            name: '台站信息',
-            type: 'line',
-            stack: '总量',
+            name: "台站信息",
+            type: "line",
+            stack: "总量",
             areaStyle: {
               normal: {}
             },
             data: this.lineUrlCount.data.station
           },
           {
-            name: '台站风光',
-            type: 'line',
-            stack: '总量',
+            name: "台站风光",
+            type: "line",
+            stack: "总量",
             areaStyle: {
               normal: {}
             },
             data: this.lineUrlCount.data.image
           },
           {
-            name: '新闻公告',
-            type: 'line',
-            stack: '总量',
+            name: "新闻公告",
+            type: "line",
+            stack: "总量",
             areaStyle: {
               normal: {}
             },
             data: this.lineUrlCount.data.news
           },
           {
-            name: '数据资源',
-            type: 'line',
-            stack: '总量',
+            name: "数据资源",
+            type: "line",
+            stack: "总量",
             areaStyle: {
               normal: {}
             },
             data: this.lineUrlCount.data.data
           },
           {
-            name: '科研成果',
-            type: 'line',
-            stack: '总量',
+            name: "科研成果",
+            type: "line",
+            stack: "总量",
             areaStyle: {
               normal: {}
             },
             data: this.lineUrlCount.data.achv
           },
           {
-            name: '科研项目',
-            type: 'line',
-            stack: '总量',
+            name: "科研项目",
+            type: "line",
+            stack: "总量",
             areaStyle: {
               normal: {}
             },
             data: this.lineUrlCount.data.project
           }
         ]
-      }
+      };
     },
     mapOption() {
       return {
-        backgroundColor: '#404a59',
+        backgroundColor: "#404a59",
         tooltip: {
-          trigger: 'item',
+          trigger: "item",
           formatter: function(data) {
-            return data.name + ':' + data.value[2]
+            return data.name + ":" + data.value[2];
           }
         },
         legend: {
-          orient: 'vertical',
-          y: 'bottom',
-          x: 'right',
-          data: ['浏览量'],
+          orient: "vertical",
+          y: "bottom",
+          x: "right",
+          data: ["浏览量"],
           textStyle: {
-            color: '#fff'
+            color: "#fff"
           }
         },
         geo: {
-          map: 'china',
+          map: "china",
           label: {
             emphasis: {
               show: false
@@ -359,25 +360,27 @@ export default {
           },
           itemStyle: {
             normal: {
-              areaColor: '#323c48',
-              borderColor: '#111'
+              areaColor: "#323c48",
+              borderColor: "#111"
             },
             emphasis: {
-              areaColor: '#2a333d'
+              areaColor: "#2a333d"
             }
           }
         },
         series: [
           {
-            name: '浏览量',
-            type: 'scatter',
-            coordinateSystem: 'geo',
+            name: "浏览量",
+            type: "scatter",
+            coordinateSystem: "geo",
             data: this.mapData,
-            symbolSize: val => val[2],
+            symbolSize: val => {
+              return Math.ceil(val[2]) * 40 / this.maxMapCount;
+            },
             label: {
               normal: {
-                formatter: '{b}',
-                position: 'right',
+                formatter: "{b}",
+                position: "right",
                 show: false
               },
               emphasis: {
@@ -386,20 +389,20 @@ export default {
             },
             itemStyle: {
               normal: {
-                color: '#ddb926'
+                color: "#ddb926"
               }
             }
           }
         ]
-      }
+      };
     }
   },
   methods: {
     //上次登录信息
     async getLastLogin() {
-      let user_id = Cookies.get('user_id')
-      this.lastLogin.username = Cookies.get('username')
-      this.lastLogin.name = Cookies.get('user')
+      let user_id = Cookies.get("user_id");
+      this.lastLogin.username = Cookies.get("username");
+      this.lastLogin.name = Cookies.get("user");
       if (user_id) {
         let query = {
           keys: {
@@ -408,17 +411,17 @@ export default {
           offset: 0,
           limit: 2,
           order: 0,
-          order_by: 'data_id'
-        }
-        let res_last_login = await this.apis.log_view.list(query)
+          order_by: "data_id"
+        };
+        let res_last_login = await this.apis.log_view.list(query);
         if (res_last_login.status === 200 && res_last_login.data.code === 1) {
-          let data = res_last_login.data.data.rows
+          let data = res_last_login.data.data.rows;
           if (data.length > 0) {
-            let index = data.length
-            this.lastLogin.time = data[index - 1].created_at
+            let index = data.length;
+            this.lastLogin.time = data[index - 1].created_at;
             this.lastLogin.city = data[index - 1].city
               ? data[index - 1].city
-              : this.lastLogin.city
+              : this.lastLogin.city;
           }
         }
       }
@@ -429,13 +432,13 @@ export default {
         type,
         from_time,
         to_time
-      }
-      let count = 0
-      let res_last_count = await this.apis.log_view.count(query)
+      };
+      let count = 0;
+      let res_last_count = await this.apis.log_view.count(query);
       if (res_last_count.status === 200 && res_last_count.data.code === 1) {
-        count = res_last_count.data.data
+        count = res_last_count.data.data;
       }
-      this.topCount[type].count = count
+      this.topCount[type].count = count;
     },
     //按模块浏览统计
     async urlViewCount(type, from_time = -7, to_time = 0) {
@@ -443,11 +446,11 @@ export default {
         type,
         from_time,
         to_time
-      }
-      let data = []
-      let res_last_count = await this.apis.log_view.url(query)
+      };
+      let data = [];
+      let res_last_count = await this.apis.log_view.url(query);
       if (res_last_count.status === 200 && res_last_count.data.code === 1) {
-        data = res_last_count.data.data
+        data = res_last_count.data.data;
         if (type === 2) {
           let lineData = {
             station: [0, 0, 0, 0, 0, 0, 0],
@@ -456,45 +459,45 @@ export default {
             data: [0, 0, 0, 0, 0, 0, 0],
             achv: [0, 0, 0, 0, 0, 0, 0],
             project: [0, 0, 0, 0, 0, 0, 0]
-          }
+          };
           for (let dd of data) {
-            let date = dd.date
-            let key = dd.name
-            let value = dd.value
-            let dateIndex = this.lineUrlCount.dateDic[date]
+            let date = dd.date;
+            let key = dd.name;
+            let value = dd.value;
+            let dateIndex = this.lineUrlCount.dateDic[date];
             if (dateIndex === undefined) {
-              continue
+              continue;
             }
             if (
               ![
-                'station',
-                'image',
-                'news',
-                'data',
-                'paper',
-                'monography',
-                'patent',
-                'awards',
-                'project'
+                "station",
+                "image",
+                "news",
+                "data",
+                "paper",
+                "monography",
+                "patent",
+                "awards",
+                "project"
               ].includes(key)
             ) {
-              continue
+              continue;
             }
-            if (['paper', 'monography', 'patent', 'awards'].includes(key)) {
-              key = 'achv'
+            if (["paper", "monography", "patent", "awards"].includes(key)) {
+              key = "achv";
             }
-            lineData[key][dateIndex] = value
+            lineData[key][dateIndex] = value;
           }
-          this.lineUrlCount.data = lineData
+          this.lineUrlCount.data = lineData;
         } else {
           let items = [],
-            values = []
+            values = [];
           for (let dd of data) {
-            items.push(dd.date)
-            values.push(dd.value)
+            items.push(dd.date);
+            values.push(dd.value);
           }
-          this.barUrlCount.items = items
-          this.barUrlCount.values = values
+          this.barUrlCount.items = items;
+          this.barUrlCount.values = values;
         }
       }
     },
@@ -504,41 +507,45 @@ export default {
         type,
         from_time,
         to_time
-      }
-      let res_last_count = await this.apis.log_view.map(query)
+      };
+      let res_last_count = await this.apis.log_view.map(query);
       if (res_last_count.status === 200 && res_last_count.data.code === 1) {
-        let data = res_last_count.data.data
-        this.tableData = data
-        var res = []
+        let data = res_last_count.data.data;
+        this.tableData = data;
+        var res = [];
         for (var i = 0; i < data.length; i++) {
-          var geoCoord = geoCoordMap[data[i].name]
+          let city = data[i];
+          var geoCoord = geoCoordMap[city.name];
           if (geoCoord) {
+            if (city.value > this.maxMapCount) {
+              this.maxMapCount = city.value;
+            }
             res.push({
-              name: data[i].name,
-              value: geoCoord.concat(data[i].value)
-            })
+              name: city.name,
+              value: geoCoord.concat(city.value)
+            });
           }
         }
-        this.mapData = res
+        this.mapData = res;
       }
     }
   },
   async mounted() {
-    let dateDic = this.filters.getLast7DateDic()
-    this.lineUrlCount.dateDic = dateDic.dic
-    this.lineUrlCount.dateItems = dateDic.date
+    let dateDic = this.filters.getLast7DateDic();
+    this.lineUrlCount.dateDic = dateDic.dic;
+    this.lineUrlCount.dateItems = dateDic.date;
     //用户信息
-    this.getLastLogin()
+    this.getLastLogin();
     //顶部统计
-    this.userViewCount(0)
-    this.userViewCount(1)
-    this.userViewCount(2)
-    this.userViewCount(3)
-    this.urlViewCount(1)
-    this.urlViewCount(2)
-    this.mapViewCount(1)
+    this.userViewCount(0);
+    this.userViewCount(1);
+    this.userViewCount(2);
+    this.userViewCount(3);
+    this.urlViewCount(1);
+    this.urlViewCount(2);
+    this.mapViewCount(1);
   }
-}
+};
 </script>
 <style lang="less">
 .line-gray {
